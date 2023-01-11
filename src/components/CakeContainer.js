61f4e2f06b89aced5a8d53c66323006ab4735566
Shimul Mahmud <shimulmahmud18@gmail.com>
2023-01-11 16:56:06 +0600

    react-redux-application

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeContainer = () => {
+  return (
+    <div>
+      <h1>Number of cakes</h1>
+      <button type="submit">Buy now</button>
+    </div>
+  );
+};
+
+export default CakeContainer;
